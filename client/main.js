23f42f3aae825ac43e4a1c12db9bae342231ef9a
CodexCodech <israel_200@outlook.com>
2020-04-12 19:07:59 -0400

    first commit

---
+++ client/main.js
@@ -0,0 +1,10 @@
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+import 'meteor/jkuester:blaze-bs4'
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.css' // this is the default BS theme as example
+import popper from 'popper.js'
+global.Popper = popper // fixes some issues with Popper and Meteor
+
+import './main.html';
+import '../lib/collection.js';
