348feb153eb00652236e5d096fbc05d735f6cb5c
CodexCodech <israel_200@outlook.com>
2020-05-12 15:11:37 -0400

    Navbar

--- client/main.js
+++ client/main.js
@@ -1,28 +1,6 @@
-import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
-import 'meteor/jkuester:blaze-bs4'
-import 'bootstrap'
-import 'bootstrap/dist/css/bootstrap.css' // this is the default BS theme as example
-import popper from 'popper.js'
-global.Popper = popper // fixes some issues with Popper and Meteor
-
-import './main.html';
-import '../lib/collection.js';
-import '../lib/accountUI.js';
-
-Template.Format.helpers({
-	Format(){
-		return listdb.find();
-	}
-
-})
+import '../imports/ui/layout.js';
 
 
 
-Template.AddaTask.events({
-	'click .js-addtask '(event, instance){	
-		$("#addtaskModal").modal("show");
-	}
 
-	
-})
+
---
+++ imports/ui/layout.js
@@ -0,0 +1,20 @@
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+import 'meteor/jkuester:blaze-bs4';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.css' ;// this is the default BS theme as example
+import popper from 'popper.js';
+global.Popper = popper // fixes some issues with Popper and Meteor
+
+//ui imports
+import './NavBar.html';
+import './layout.html';
+
+
+
+
+
+
+// api imports
+import '../../lib/collection.js';
+import '../../lib/accountUI.js';
