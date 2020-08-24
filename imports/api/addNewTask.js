51849958beb3d8e399b87cf16451054ad1529315
Israel Lewis <israel_200@outlook.com>
2020-08-23 21:13:22 -0400

    Added filter, show priv task

--- imports/api/addNewTask.js
+++ imports/api/addNewTask.js
@@ -15,6 +15,7 @@ Template.AddaTask.events({
 	'click .js-saveTask'(event, instance){
 		var thetasktitle =$('#taskttl').val();
 		var theTask =$('#Task').val();
+		var pubLIC =$('#defaultCheck1').prop('checked');
 		console.log("Saving Title " + thetasktitle);
 		console.log("Saving New Task of " + theTask);
 		
@@ -22,8 +23,11 @@ Template.AddaTask.events({
 				"title": thetasktitle,
 				"task": theTask,
 				"owner": Meteor.userId(),
-				"createdOn": new Date().getTime()
+				"createdOn": new Date().getTime(),
+				"publictask": pubLIC	
 			});
+		$("#taskttl").val("");
+		$("#Task").val("");
 	},
 
 	'click .js-closeTaskModal'(event, instance){
--- imports/api/mainBody.js
+++ imports/api/mainBody.js
@@ -1,6 +1,21 @@
 Template.MainBody.helpers({
 	pbTask(){
-		return listdb.find({});
+		if(Meteor.userId()){
+			return listdb.find( { $or: [ { owner: Meteor.userId()}, { publictask: true } ] } )
+		}
+		return listdb.find({publictask:true});
+	},
+	Tpub(){
+		if(this.publictask==true)
+			return true;
+		else 
+			return false;
+	},
+	TcomP(){
+		if(this.comp==true)
+			return true;
+		else	
+			return false;
 	}
 
 });
